f53b92b2a10feb7e7389280717c31f62fd311299
Kaan Ince <kaaniince@outlook.com>
2024-11-17 14:06:51 +0300

    today design changed

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -6,10 +6,12 @@ const WeatherCard = ({ day }) => {
   const options = { weekday: "long" }; // Gün adı
   const dayName = date.toLocaleDateString("tr-TR", options);
 
+  const today = new Date().toLocaleDateString("tr-TR", { weekday: "long" });
+
   const iconUrl = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
 
   return (
-    <div className="weather-card">
+    <div className={`weather-card ${dayName === today ? "today" : ""}`}>
       <h3>{dayName}</h3>
       <img src={iconUrl} alt={day.weather[0].description} />
       <p>{`Max: ${Math.round(day.main.temp_max)}°C`}</p>
